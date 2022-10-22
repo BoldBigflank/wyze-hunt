031a8df6d636f6bec3f2337092db5500a1b9cd6d
Alex Swan <alex@bold-it.com>
2022-10-21 20:25:58 -0500

    sqlite3 flights storage

--- scripts/add_current_flight_positions.js
+++ scripts/add_current_flight_positions.js
@@ -1,19 +1,49 @@
 import * as dotenv from 'dotenv'
 import sqlite3 from 'sqlite3'
 import fetch from 'cross-fetch'
+import chalk from 'chalk'
 dotenv.config()
 
-const db = new sqlite3.Database('./db')
+const db = new sqlite3.Database('./flights.db')
 
 const FA_API_KEY=process.env.FA_API_KEY
 
 const FA_SERVER='https://aeroapi.flightaware.com/aeroapi'
 
 const main = async () => {
-    const positions = await getFlightData()
-    console.log(`found ${positions.length} positions`)
-    // console.log(JSON.stringify(positions, null, 2))
+    prepareDatabase()
+    const flights = await getFlightData()
+    console.log(chalk.yellow(`Found ${flights.length} flights`))
     // Store them in the DB
+    insertIntoDatabase(flights)
+}
+
+const insertIntoDatabase = (flights) => {
+    const columns = Object.keys(flights[0])
+    const values = flights.map((flight) => {
+        const vs = columns.map((c) => typeof flight[c] == 'number' ? flight[c] : `'${flight[c]}'`).join(',')
+        return `(${vs})`
+    })
+    const insertSQL = `INSERT INTO flights(${columns.join(',')})
+    VALUES ${values.join(',')};`
+    db.run(insertSQL)
+    console.log(chalk.blue(`Inserted ${values.length} records`))
+}
+
+const prepareDatabase = () => {
+    db.run(`CREATE TABLE IF NOT EXISTS flights (
+        id INTEGER PRIMARY KEY,
+        fa_flight_id TEXT NOT NULL,
+        altitude INTEGER NOT NULL,
+        altitude_change TEXT NOT NULL,
+        groundspeed INTEGER NOT NULL,
+        heading INTEGER NOT NULL,
+        latitude REAL NOT NULL,
+        longitude REAL NOT NULL,
+        timestamp TEXT NOT NULL,
+        update_type TEXT NOT NULL
+    );`);
+
 }
 
 const getFlightData = async () => {
@@ -39,7 +69,7 @@ const getFlightData = async () => {
             const qs = new URLSearchParams(rtQuery)
             url = `${FA_SERVER}/flights/search/positions?${qs.toString()}`
         }
-        console.log(url)
+        console.log(chalk.yellow(url))
         const result = await fetch(url, {
             headers: {
                 'x-apikey': FA_API_KEY,
